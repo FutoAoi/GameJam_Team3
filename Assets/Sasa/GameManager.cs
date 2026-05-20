93c2411a064b3be388b2f55c9227227742ac5f0a
sasa <ryoga1231.pittch@gmail.com>
2026-05-20 14:19:09 +0900

    [Add]リザルトシステム

---
+++ Assets/Sasa/GameManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static int score;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        score = 65;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
