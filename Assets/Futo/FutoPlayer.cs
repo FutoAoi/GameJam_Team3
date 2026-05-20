e21246dd284ec4940d62f1eb0d8728ac71108cfd
FutoAoi <Faoi0803@icloud.com>
2026-05-20 16:31:36 +0900

    [Add]ステージのテンプレートの作成

--- Assets/Futo/FutoPlayer.cs
+++ Assets/Futo/FutoPlayer.cs
@@ -1,12 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FutoPlayer : MonoBehaviour
 {
-    [SerializeField] private float _playerSpeed;
-    private Rigidbody _rb;
-    void Start()
+    [SerializeField] private float _moveSpeed = 5f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
+    // PlayerInput ‚©‚çŒÄ‚Î‚ê‚é
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+
+        rb.linearVelocity = new Vector3(
+            move.x * _moveSpeed,
+            rb.linearVelocity.y,
+            move.z * _moveSpeed
+        );
+    }
 }
